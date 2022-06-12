b09ece1f053927e25c8ba342c1e69286ca2dca7b
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-13 00:23:08 +0200

    Refactor Form and App

--- src/App.js
+++ src/App.js
@@ -8,17 +8,6 @@ import { useState } from 'react';
 function App() {
   const [randomJoke, setRandomJoke] = useState(null);
 
-  async function getRandomJoke(url) {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setRandomJoke(data);
-      console.log(randomJoke);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <Grid>
       <Title>Chuck Norris Random Jokes</Title>
@@ -34,6 +23,17 @@ function App() {
       <footer>Links to API</footer>
     </Grid>
   );
+
+  async function getRandomJoke(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setRandomJoke(data);
+      console.log(randomJoke);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,34 @@
 import Container from './Container.styled.js';
-import { useEffect, useState } from 'react';
 
 export default function Form({ getRandomJoke }) {
-  const [categories, setCategories] = useState([]);
+  const categories = [
+    'animal',
+    'career',
+    'dev',
+    'fashion',
+    'food',
+    'history',
+    'money',
+    'music',
+    'science',
+    'sport',
+    'travel',
+  ];
 
-  async function getCategories() {
-    try {
-      const url = 'https://api.chucknorris.io/jokes/categories';
-      const response = await fetch(url);
-      const data = await response.json();
-      setCategories(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    getCategories();
-  }, []);
+  return (
+    <Container onSubmit={handleSubmit}>
+      <h2>Get Random Joke</h2>
+      <label htmlFor="name">Enter your name</label>
+      <input id="name" type="text" name="name" />
+      <label htmlFor="categories">Get Random Joke by Category</label>
+      <select name="categories" id="categories">
+        {categories.map((category, index) => {
+          return <option key={index}>{category}</option>;
+        })}
+      </select>
+      <button type="submit">Get Random Joke</button>
+    </Container>
+  );
 
   function generateUrl(name, category) {
     if (name && category) {
@@ -41,19 +52,4 @@ export default function Form({ getRandomJoke }) {
     console.log(url);
     getRandomJoke(url);
   }
-
-  return (
-    <Container onSubmit={handleSubmit}>
-      <h2>Get Random Joke</h2>
-      <label htmlFor="name">Enter your name</label>
-      <input id="name" type="text" name="name" />
-      <label htmlFor="categories">Get Random Joke by Category</label>
-      <select name="categories" id="categories">
-        {categories.map((category, index) => {
-          return <option key={index}>{category}</option>;
-        })}
-      </select>
-      <button type="submit">Get Random Joke</button>
-    </Container>
-  );
 }
