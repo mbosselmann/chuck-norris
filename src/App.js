a29e10bc042cff5c26d14a94fbe6e38c69cbbb86
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-13 00:13:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,57 @@
+import styled from 'styled-components';
+import Form from './components/Form.js';
+import Search from './components/Search.js';
+import Title from './components/Title.styled.js';
+import Joke from './components/Joke.js';
+import { useState } from 'react';
+
 function App() {
-  return <div></div>;
+  const [randomJoke, setRandomJoke] = useState(null);
+
+  async function getRandomJoke(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setRandomJoke(data);
+      console.log(randomJoke);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Grid>
+      <Title>Chuck Norris Random Jokes</Title>
+      <main>
+        <section>
+          Explanation Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Fugiat, ratione?
+        </section>
+        {randomJoke && <Joke randomJoke={randomJoke} />}
+        <Form getRandomJoke={getRandomJoke} />
+        <Search />
+      </main>
+      <footer>Links to API</footer>
+    </Grid>
+  );
 }
 
 export default App;
+
+const Grid = styled.div`
+  height: 100%;
+  max-width: 530px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: 100px 1fr 100px;
+
+  main {
+    overflow-y: scroll;
+    padding: 20px;
+  }
+
+  footer {
+    background-color: gold;
+    color: white;
+  }
+`;
