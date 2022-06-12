cbd71a312d4600c64a8dc2947c5566e89ddf34f3
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-13 00:13:35 +0200

    Update index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import GlobalStyles from './GlobalStyles.js';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
