1ba8aefda88afa420a3092e88136609292120f88
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-17 23:51:12 +0200

    Fix form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export default function Form({ getRandomFact }) {
   const categories = [
+    'no category selected',
     'animal',
     'career',
     'dev',
-    'fashion',
     'food',
     'history',
     'money',
@@ -17,29 +17,43 @@ export default function Form({ getRandomFact }) {
 
   return (
     <Container onSubmit={handleSubmit} aria-labelledby="title">
-      <label htmlFor="name">Enter your name</label>
-      <input id="name" type="text" name="name" />
-      <label htmlFor="categories">Get Random Fact by Category</label>
-      <select name="categories" id="categories">
-        {categories.map((category, index) => {
-          return <option key={index}>{category}</option>;
-        })}
-      </select>
-      <button type="submit">Get Random Fact</button>
+      <p>
+        <label htmlFor="name">Your name to get a customised fact</label>
+        <input id="name" type="text" name="name" />
+      </p>
+      <p>
+        <label htmlFor="categories">Select category to get a fact from</label>
+        <select name="categories" id="categories">
+          {categories.map((category, index) => {
+            return (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            );
+          })}
+        </select>
+      </p>
+      <Box>
+        <Button type="submit">Get Random Fact</Button>
+      </Box>
     </Container>
   );
 
   function generateUrl(name, category) {
+    if (!name) {
+      return 'https://api.chucknorris.io/jokes/random?category=animal,career,dev,food,history,money,music,science,sport,travel';
+    }
+    if (name && category === 'no category selected') {
+      return `https://api.chucknorris.io/jokes/random?name=${name}&category=animal,career,dev,food,history,money,music,science,sport,travel`;
+    }
     if (name && category) {
       return `https://api.chucknorris.io/jokes/random?name=${name}&category=${category}`;
     }
-    if (name) {
-      return `https://api.chucknorris.io/jokes/random?name=${name}`;
+    if (category === 'no category selected') {
+      return 'https://api.chucknorris.io/jokes/random?category=animal,career,dev,food,history,money,music,science,sport,travel';
     }
     if (category) {
       return `https://api.chucknorris.io/jokes/random?category=${category}`;
-    } else {
-      return 'https://api.chucknorris.io/jokes/random';
     }
   }
 
@@ -48,7 +62,6 @@ export default function Form({ getRandomFact }) {
     const form = event.target;
     const { name, categories } = form.elements;
     const url = generateUrl(name.value, categories.value);
-    console.log(url);
     getRandomFact(url);
   }
 }
@@ -56,6 +69,37 @@ export default function Form({ getRandomFact }) {
 const Container = styled.form`
   display: flex;
   flex-direction: column;
+  align-content: flex-start;
   gap: 20px;
-  margin-bottom: 45px;
+  padding: 20px 0 35px;
+
+  p {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    margin: 0;
+  }
+`;
+
+const Button = styled.button`
+  background-color: var(--secondary-color);
+  border: none;
+  border-radius: 5px;
+  font-family: 'Bangers', cursive;
+  font-size: 1.5rem;
+  width: 100%;
+  position: absolute;
+  top: -4px;
+
+  &:active {
+    top: 0;
+  }
+`;
+
+const Box = styled.div`
+  background-color: var(--dark-color);
+  position: relative;
+  height: 3rem;
+  border-radius: 5px;
+  margin-top: 5px;
 `;
