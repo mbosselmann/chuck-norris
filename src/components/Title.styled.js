52f7912d5ac1741de845eed547d55069099e0d0e
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-17 23:50:25 +0200

    Adjust title

--- src/components/Title.styled.js
+++ src/components/Title.styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const Title = styled.h1`
-  border-bottom: 5px solid gold;
+  border-bottom: 5px solid var(--secondary-color);
   display: inline-block;
   font-family: 'Bangers', cursive;
-  font-size: 48px;
+  font-size: 2.5rem;
   text-align: center;
-  margin: 10px 0;
+  margin: 10px;
 `;
 
 export default Title;
