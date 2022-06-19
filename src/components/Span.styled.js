010a42407c27068776e2503673afd880e20dd740
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-19 20:14:25 +0200

    Adjust span and title component

--- src/components/Span.styled.js
+++ src/components/Span.styled.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Span = styled.span`
   font-weight: bold;
-  color: var(--dark-color);
   font-size: 1.3rem;
 `;
 
--- src/components/Title.styled.js
+++ src/components/Title.styled.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 const Title = styled.h1`
-  border-bottom: 5px solid var(--secondary-color);
   display: inline-block;
   font-family: 'Bangers', cursive;
-  font-size: 2.5rem;
+  font-size: 2.8rem;
   text-align: center;
   margin: 10px;
-  letter-spacing: 1px;
+  letter-spacing: 3px;
   padding: 5px 10px;
+  text-shadow: 4px 3px 0px var(--secondary-color);
 `;
 
 export default Title;
